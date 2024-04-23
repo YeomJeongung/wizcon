9bd1bf9dbf7014908234ffece29c3f7e16f01ea6
YeomJeongung <yjw1404@wiz-con.com>
2024-04-23 15:46:20 +0900

    modify

--- src/components/mainPart1.vue
+++ src/components/mainPart1.vue
@@ -1,31 +1,47 @@
 <script setup>
 //import { ref } from 'vue';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import 'swiper/css'
 
-const swiperModules = [Autoplay, Pagination, Navigation];
+const swiperModules = [Autoplay, Pagination, Navigation]
 
 const onSwiper = () => {
   //
-};
+}
 
 const onSlideChange = () => {
   //
 }
-
 </script>
 <template>
   <div class="defpd">
     <div class="part1-txt">
-      <div class="font1 mb2 w2animation animation-duration-1000 animation-delay-500 animation-ease-in-out" 
-      v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }">SHINING Association</div>
-      <div class="font3-5 bold lineh w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out" 
-      v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }">WELCOME TO</div>
-      <div class="font3-5 bold red bold8 w2animation animation-duration-1000 animation-delay-1200 animation-ease-in-out" 
-      v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }">OUR ASSOCIATION</div>
-      <div class="font1 mt2 w2animation animation-duration-1000 animation-delay-1500 animation-ease-in-out" 
-      v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }">회원들의 전문적 지식과 기술을 향상시키고, 지속 가능한 사회 발전을 위한 노력을 기울이고 있습니다.</div>
+      <div
+        class="font1 mb2 w2animation animation-duration-1000 animation-delay-500 animation-ease-in-out"
+        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }"
+      >
+        DREAM IT! MAKE IT HAPPEN
+      </div>
+      <div
+        class="font3-5 bold lineh w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }"
+      >
+        WELCOME TO
+      </div>
+      <div
+        class="font3-5 bold red bold8 w2animation animation-duration-1000 animation-delay-1200 animation-ease-in-out"
+        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }"
+      >
+        WIZCON
+      </div>
+      <div
+        class="font1 mt2 w2animation animation-duration-1000 animation-delay-1500 animation-ease-in-out"
+        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft' }"
+      >
+        회원들의 전문적 지식과 기술을 향상시키고, 지속 가능한 사회 발전을 위한 노력을 기울이고
+        있습니다.
+      </div>
     </div>
   </div>
   <swiper
@@ -34,14 +50,15 @@ const onSlideChange = () => {
     :speed="1000"
     @swiper="onSwiper"
     @slideChange="onSlideChange"
-    :pagination="{ clickable: true,
+    :pagination="{
+      clickable: true,
       renderBullet: function (index, className) {
-          return `<span class='${className} custom_blul1' data='${index}'></span>`;
-        },
+        return `<span class='${className} custom_blul1' data='${index}'></span>`
+      }
     }"
     :autoplay="{
       delay: 2500,
-      disableOnInteraction: false,
+      disableOnInteraction: false
     }"
     :modules="swiperModules"
     class="h100vh"
@@ -55,5 +72,4 @@ const onSlideChange = () => {
   </swiper>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
--- src/components/mainPart2.vue
+++ src/components/mainPart2.vue
@@ -32,10 +32,10 @@ const subslider = ref([
   }
 ])
 
-import image1 from '@/assets/images/sample2.jpeg'
-import image2 from '@/assets/images/sample.jpg'
-import image3 from '@/assets/images/sample3.jpg'
-import image4 from '@/assets/images/sample4.jpg'
+import image1 from '@/assets/images/business.jpg'
+import image2 from '@/assets/images/business.jpg'
+import image3 from '@/assets/images/business.jpg'
+import image4 from '@/assets/images/business.jpg'
 
 const imgPath = [image1, image2, image3, image4]
 
