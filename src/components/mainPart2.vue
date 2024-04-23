72a9a01838f48460fef20e932f1778ca147dcc56
YeomJeongung <yjw1404@wiz-con.com>
2024-04-23 14:09:36 +0900

    modify

--- src/components/mainPart2.vue
+++ src/components/mainPart2.vue
@@ -1,92 +1,108 @@
 <script setup>
-import { ref } from 'vue';
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Pagination, Navigation } from 'swiper/modules';
+import { ref } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Pagination, Navigation } from 'swiper/modules'
 
-const swiperModules = [Pagination, Navigation];
+const swiperModules = [Pagination, Navigation]
 
 const subslider = ref([
   {
-    etitle : 'business1',
-    ktitle : '비즈니스 네트워킹',
-    img : '/src/assets/images/sample2.jpeg',
-    contents : '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
+    etitle: 'business1',
+    ktitle: '비즈니스 네트워킹',
+    contents:
+      '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
   },
   {
-    etitle : 'business2',
-    ktitle : '비즈니스 네트워킹',
-    img : '/src/assets/images/sample.jpg',
-    contents : '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
+    etitle: 'business2',
+    ktitle: '비즈니스 네트워킹',
+    contents:
+      '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
   },
   {
-    etitle : 'business3',
-    ktitle : '비즈니스 네트워킹',
-    img : '/src/assets/images/sample3.jpg',
-    contents : '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
-  }
-  ,
+    etitle: 'business3',
+    ktitle: '비즈니스 네트워킹',
+    contents:
+      '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
+  },
   {
-    etitle : 'business3',
-    ktitle : '비즈니스 네트워킹',
-    img : '/src/assets/images/sample3.jpg',
-    contents : '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
+    etitle: 'business3',
+    ktitle: '비즈니스 네트워킹',
+    contents:
+      '여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요? 여기에 글을 적어요. 어떤내용이 들어갈까요?'
   }
-]);
+])
 
-//mouse 오버시 
-const targets = ref([]);
-const overEl = ref([]);
-const targetIdxRef = ref(null);
+import image1 from '@/assets/images/sample2.jpeg'
+import image2 from '@/assets/images/sample.jpg'
+import image3 from '@/assets/images/sample3.jpg'
+import image4 from '@/assets/images/sample4.jpg'
+
+const imgPath = [image1, image2, image3, image4]
+
+//mouse 오버시
+const targets = ref([])
+const overEl = ref([])
+const targetIdxRef = ref(null)
 
 const handleMouseMove = (event) => {
-const targetIndex = targets.value.findIndex(target => target.contains(event.target));
-overEl.value = false;
+  const targetIndex = targets.value.findIndex((target) => target.contains(event.target))
+  overEl.value = false
   if (targetIndex !== -1) {
-    targetIdxRef.value = targetIndex;
+    targetIdxRef.value = targetIndex
   }
-};
-const handleMouseLeave = () => {
-  targetIdxRef.value = null;
 }
-
+const handleMouseLeave = () => {
+  targetIdxRef.value = null
+}
 </script>
 <template>
   <div class="box1-t contents flx-col flx-l">
-      <h2 class="w2animation animation-duration-1000 animation-delay-500 animation-ease-in-out"
-      v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft opacity0' }"
+    <h2
+      class="w2animation animation-duration-1000 animation-delay-500 animation-ease-in-out"
+      v-animateonscroll.once="{
+        enterClass: 'fadeinleft opacity0',
+        leaveClass: 'fadeoutleft opacity0'
+      }"
+    >
+      <span class="red-line">사업안내</span>
+    </h2>
+
+    <div class="pdl11">
+      <div
+        class="font2-5 bold lineh w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+        v-animateonscroll.once="{
+          enterClass: 'fadeinleft opacity0',
+          leaveClass: 'fadeoutleft opacity0'
+        }"
       >
-        <span class="red-line">사업안내</span>
-      </h2>
-      
-
-      <div class="pdl11">
-        <div class="font2-5 bold lineh w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
-        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft opacity0' }"
-        >
-          협력과 지식 공유를 촉진하여<br />전반적인 산업 환경을 개선하겠습니다.
-        </div>
-
-        <div class="font1 mt3 color999 w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
-        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft opacity0' }"
-        >
-          환경 친화적인 기술과 관행을 촉진하며, 사회적 책임을 다하는 기업 문화를 선도합니다.
-        </div>
+        협력과 지식 공유를 촉진하여<br />전반적인 산업 환경을 개선하겠습니다.
       </div>
+
+      <div
+        class="font1 mt3 color999 w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+        v-animateonscroll.once="{
+          enterClass: 'fadeinleft opacity0',
+          leaveClass: 'fadeoutleft opacity0'
+        }"
+      >
+        환경 친화적인 기술과 관행을 촉진하며, 사회적 책임을 다하는 기업 문화를 선도합니다.
+      </div>
+    </div>
   </div>
 
-
-  <div class="box1-b nodrag subslider animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+  <div
+    class="box1-b nodrag subslider animation-duration-1000 animation-delay-1000 animation-ease-in-out"
     v-animateonscroll.once="{ enterClass: 'fadein opacity0', leaveClass: 'fadeout opacity0' }"
-    >
-      <div @mouseleave="handleMouseLeave">
-        <swiper
+  >
+    <div @mouseleave="handleMouseLeave">
+      <swiper
         :slides-per-view="3"
         :space-between="0"
         :speed="500"
         :navigation="true"
         :autoplay="{
           delay: 3500,
-          disableOnInteraction: false,
+          disableOnInteraction: false
         }"
         class=""
         :modules="swiperModules"
@@ -98,20 +114,17 @@ const handleMouseLeave = () => {
                 <h3>{{ item.ktitle }}</h3>
                 <span>{{ item.etitle }}</span>
               </div>
-              <div :class="[targetIdxRef != idx ? 'off':'on','info-bottom']" >
+              <div :class="[targetIdxRef != idx ? 'off' : 'on', 'info-bottom']">
                 <span>{{ item.contents }}</span>
               </div>
-              <div class="item" :style="`background-image: url(${ item.img })`"></div>
-              <div ref="overEl" :class="[targetIdxRef != idx ? 'off':'on','overEl']"></div>
+              <div class="item" :style="`background-image: url(${imgPath[idx]})`"></div>
+              <div ref="overEl" :class="[targetIdxRef != idx ? 'off' : 'on', 'overEl']"></div>
             </div>
           </swiper-slide>
         </template>
       </swiper>
-      </div>
     </div>
-
-
+  </div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
--- src/components/mainPart3.vue
+++ src/components/mainPart3.vue
@@ -1,71 +1,100 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, reactive } from 'vue'
 const netitem = ref([
   {
-    etitle : 'DEVELOPMENT AND NETWORKING',
-    ktitle : '비즈니스 네트워킹',
-    img : '/src/assets/images/icon.svg'
+    etitle: 'DEVELOPMENT AND NETWORKING',
+    ktitle: '비즈니스 네트워킹'
   },
   {
-    etitle : 'TRAINING PROGRAMS',
-    ktitle : '교육 및 훈련 프로그램',
-    img : '/src/assets/images/icon.svg'
+    etitle: 'TRAINING PROGRAMS',
+    ktitle: '교육 및 훈련 프로그램'
   },
   {
-    etitle : 'PROVISION OF DATA',
-    ktitle : '정보 및 자료 제공',
-    img : '/src/assets/images/icon.svg'
+    etitle: 'PROVISION OF DATA',
+    ktitle: '정보 및 자료 제공'
   }
-]);
+])
 
-//mouse 오버시 
-const targets = ref([]);
-const targetOffsets = reactive([]);
-const targetIdxRef = ref(0);
-const mouseleaveOff = ref(true);
+import image1 from '@/assets/images/icon.svg'
+import image2 from '@/assets/images/icon.svg'
+import image3 from '@/assets/images/icon.svg'
+
+const imgPath = [image1, image2, image3]
+
+//mouse 오버시
+const targets = ref([])
+const targetOffsets = reactive([])
+const targetIdxRef = ref(0)
+const mouseleaveOff = ref(true)
 
 const handleMouseMove = (event) => {
-const targetIndex = targets.value.findIndex(target => target.contains(event.target));
-mouseleaveOff.value = false;
+  const targetIndex = targets.value.findIndex((target) => target.contains(event.target))
+  mouseleaveOff.value = false
   if (targetIndex !== -1) {
-    const parentElement = targets.value[targetIndex].parentNode;
-    const targetRect = targets.value[targetIndex].getBoundingClientRect();
-    const relativeLeft = targetRect.left - parentElement.getBoundingClientRect().left;
-    targetOffsets[targetIndex] = relativeLeft;
-    targetIdxRef.value = targetIndex;
+    const parentElement = targets.value[targetIndex].parentNode
+    const targetRect = targets.value[targetIndex].getBoundingClientRect()
+    const relativeLeft = targetRect.left - parentElement.getBoundingClientRect().left
+    targetOffsets[targetIndex] = relativeLeft
+    targetIdxRef.value = targetIndex
   }
-};
+}
 const handleMouseLeave = () => {
-  mouseleaveOff.value = true;
+  mouseleaveOff.value = true
 }
 </script>
 <template>
   <div class="part3-wrap box1">
     <div class="part3-txt flx-col flx-v">
-      <h2 class="flx-row flx-v flx-c w2animation animation-duration-1000 animation-delay-500 animation-ease-in-out"
-      v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft opacity0' }"
+      <h2
+        class="flx-row flx-v flx-c w2animation animation-duration-1000 animation-delay-500 animation-ease-in-out"
+        v-animateonscroll.once="{
+          enterClass: 'fadeinleft opacity0',
+          leaveClass: 'fadeoutleft opacity0'
+        }"
       >
         <span class="red-line">서비스분야</span>
       </h2>
 
-      <div class="flx-row flx-v flx-c font2-5 bold lineh w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
-        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft opacity0' }"
-        >
-          <span class="txt-center mt3">협력과 지식 공유를 촉진하여<br />전반적인 산업 환경을 개선하겠습니다.</span>
-        </div>
-        <div class="flx-row flx-v flx-c font1 mt2 colorddd w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
-        v-animateonscroll.once="{ enterClass: 'fadeinleft opacity0', leaveClass: 'fadeoutleft opacity0' }"
-        >
-        <span class="txt-center">환경 친화적인 기술과 관행을 촉진하며, 사회적 책임을 다하는 기업 문화를 선도합니다.</span>
-      </div>
-      <div class="nodrag blackbox flx-row animation-duration-1000 animation-delay-1000 animation-ease-in-out"
-      v-animateonscroll.once="{ enterClass: 'fadein opacity0', leaveClass: 'fadeout opacity0' }"
-      @mouseleave="handleMouseLeave"
+      <div
+        class="flx-row flx-v flx-c font2-5 bold lineh w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+        v-animateonscroll.once="{
+          enterClass: 'fadeinleft opacity0',
+          leaveClass: 'fadeoutleft opacity0'
+        }"
       >
-        <div :class="[mouseleaveOff ? 'off':'on','overEl2']" :style="{ left:  targetOffsets[targetIdxRef] + 'px'}"></div>
-        <div ref="targets" @mousemove="handleMouseMove" class="item flx-col flx-v" v-for="(item, idx) in netitem" :key="idx">
+        <span class="txt-center mt3"
+          >협력과 지식 공유를 촉진하여<br />전반적인 산업 환경을 개선하겠습니다.</span
+        >
+      </div>
+      <div
+        class="flx-row flx-v flx-c font1 mt2 colorddd w2animation animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+        v-animateonscroll.once="{
+          enterClass: 'fadeinleft opacity0',
+          leaveClass: 'fadeoutleft opacity0'
+        }"
+      >
+        <span class="txt-center"
+          >환경 친화적인 기술과 관행을 촉진하며, 사회적 책임을 다하는 기업 문화를 선도합니다.</span
+        >
+      </div>
+      <div
+        class="nodrag blackbox flx-row animation-duration-1000 animation-delay-1000 animation-ease-in-out"
+        v-animateonscroll.once="{ enterClass: 'fadein opacity0', leaveClass: 'fadeout opacity0' }"
+        @mouseleave="handleMouseLeave"
+      >
+        <div
+          :class="[mouseleaveOff ? 'off' : 'on', 'overEl2']"
+          :style="{ left: targetOffsets[targetIdxRef] + 'px' }"
+        ></div>
+        <div
+          ref="targets"
+          @mousemove="handleMouseMove"
+          class="item flx-col flx-v"
+          v-for="(item, idx) in netitem"
+          :key="idx"
+        >
           <span class="icon">
-            <img :src="`${ item.img }`" width="70" />
+            <img :src="`${imgPath[idx]}`" width="70" />
           </span>
           <div class="txt flx-col flx-v">
             <span>{{ item.etitle }}</span>
@@ -78,5 +107,4 @@ const handleMouseLeave = () => {
   </div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
